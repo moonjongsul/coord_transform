c9dca722e3cee4e72052799fa4f4badbdfaedd08
moonjongsul <moonjongsul@gmail.com>
2023-05-25 18:09:32 +0900

    값 출력 옵션 추가 (unit)

--- coord_transform/matrix.py
+++ coord_transform/matrix.py
@@ -3,69 +3,94 @@ from .transform import *
 class Matrix:
     def __init__(self):
 
-        self.r_vec    = None
-        self.t_vec    = None
+        self.rvec    = None
+        self.tvec    = None
         self.T        = None
         self.invT     = None
 
-    def from_r_euler(self, r_vec):
-        if isinstance(r_vec, list):
-            self.r_vec = np.array(r_vec)
+    def set_rvec(self, rvec, unit: str):
+        if isinstance(rvec, list):
+            self.rvec = np.array(rvec)
         else:
-            self.r_vec = r_vec
+            self.rvec = rvec
 
-        self.r_vec = deg2rad(self.r_vec)
-
-        if self.t_vec is not None:
-            self.T = get_T(self.r_vec, self.t_vec)
-            self.invT = get_inv(self.T)
-
-    def from_r_vector(self, r_vec):
-        if isinstance(r_vec, list):
-            self.r_vec = np.array(r_vec)
+        if unit == 'deg':
+            self.rvec = deg2rad(self.rvec)
+        elif unit == 'rad':
+            self.rvec = self.rvec
         else:
-            self.r_vec = r_vec
+            self.rvec = None
+            raise ValueError("Select unit = 'deg' or 'rad'")
 
-        if self.t_vec is not None:
-            self.T = get_T(self.r_vec, self.t_vec)
-            self.invT = get_inv(self.T)
+        self.update()
 
-    def from_t_vector(self, t_vec):
-        if isinstance(t_vec, list):
-            self.t_vec = np.array(t_vec)
+    def set_tvec(self, tvec, unit: str = 'm'):
+        if isinstance(tvec, list):
+            self.tvec = np.array(tvec)
         else:
-            self.t_vec = t_vec
+            self.tvec = tvec
 
-        if self.r_vec is not None:
-            self.T = get_T(self.r_vec, self.t_vec)
-            self.invT = get_inv(self.T)
+        if unit == 'm':
+            pass
+        elif unit == 'cm':
+            self.tvec /= 100.0
+        elif unit == 'mm':
+            self.tvec /= 1000.0
+        else:
+            self.tvec = None
+            raise ValueError("Select unit = 'm' or 'cm' or 'mm'")
 
-    def from_T(self, T):
+        self.update()
+
+    def set_T(self, T):
         self.T = T
-        self.invT = get_inv(T)
 
-        self.r_vec, self.t_vec = get_rt(T)
+        self.update()
 
-    def from_invT(self, invT):
+    def set_invT(self, invT):
         self.invT = invT
-        self.T = get_inv(invT)
 
-        self.r_vec, self.t_vec = get_rt(self.T)
+        self.update()
 
-    def get_r_vector(self):
-        return self.r_vec
-
-    def get_r_euler(self):
-        return rad2deg(self.r_vec)
+    def get_rvec(self, unit: str):
+        if unit == 'deg':
+            return rad2deg(self.rvec)
+        elif unit == 'rad':
+            return self.rvec
+        else:
+            raise ValueError("Select unit = 'deg' or 'rad'")
 
     def get_r_matrix(self):
         return self.T[:3, :3]
 
-    def get_t_vector(self):
-        return self.t_vec
+    def get_tvec(self, unit: str = 'm'):
+        if unit == 'm':
+            return self.tvec
+        elif unit == 'cm':
+            return self.tvec * 100.0
+        elif unit == 'mm':
+            return self.tvec * 1000.0
+        else:
+            raise ValueError("Select unit = 'm' or 'cm' or 'mm'")
 
     def get_T(self):
         return self.T
 
     def get_inv(self):
         return self.invT
+
+    def update(self):
+        if self.rvec is not None and self.tvec is not None:
+            self.T = get_T(self.rvec, self.tvec)
+            self.invT = get_inv(self.T)
+
+        if self.rvec is None and self.tvec is None and self.T is not None:
+            self.rvec, self.tvec = get_rt(self.T)
+            if self.invT is None:
+                self.invT = get_inv(self.T)
+
+        if self.rvec is None and self.tvec is None and self.invT is not None:
+            if self.T is None:
+                self.T = get_inv(self.invT)
+
+            self.rvec, self.tvec = get_rt(self.T)
